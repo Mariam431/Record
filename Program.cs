e587d741ee0bca97eaefcfd074be112c74679b27
mariam <mariam@MYPROBOOK>
2023-04-29 14:32:29 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+
+        public record Person(string FirstName, string LastName, string[] PhoneNumbers);
+
+        public static void Main()
+        {
+            var phoneNumbers = new string[2];
+            Person person1 = new("Nancy", "Davio", phoneNumbers);
+            Person person2 = new("Nancy", "Davio", phoneNumbers);
+            Console.WriteLine(person1 == person2); // output: True
+
+            person1.PhoneNumbers[0] = "555-1234";
+            Console.WriteLine(person1 == person2); // output: True
+
+            Console.WriteLine(ReferenceEquals(person1, person2)); // output: False
+        }
+
+
+    }
+}
